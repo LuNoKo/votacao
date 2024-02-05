cdf39bc6a3f811e35a98cc7a4be77610f2fd73a6
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2024-02-05 17:29:04 -0300

    Add TypeOrm and conection to database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DATA_BASE_DATABASE,
+      host: process.env.DATA_BASE_HOST,
+      username: process.env.DATA_BASE_USERNAME,
+      password: process.env.DATA_BASE_PASSWORD,
+      port: Number(process.env.DATA_BASE_PORT),
+      synchronize: false,
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [`${__dirname}/migration/{.ts,*.js}`],
+      migrationsRun: true,
+      verboseRetryLog: true,
+    }),
   ],
   controllers: [],
   providers: [
