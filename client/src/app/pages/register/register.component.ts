b83dea66b9d0232a186fb89dc6e8cc522c3eb52e
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2024-02-06 18:01:50 -0300

    Add register page

---
+++ client/src/app/pages/register/register.component.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { UsersService } from '../../services/users/users.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  providers: [provideNgxMask()],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  registerForm!: FormGroup;
+
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UsersService
+  ) {}
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      cpf: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get dadosForm() {
+    return this, this.registerForm.controls;
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.userService
+        .registerUser({
+          name: this.dadosForm['name'].value,
+          cpf: this.dadosForm['cpf'].value,
+          password: this.dadosForm['password'].value,
+        })
+        .subscribe({
+          next: (response) => {
+            this.router.navigate(['/']);
+          },
+          error: (error) => {
+            alert(error);
+          },
+        });
+    }
+  }
+}
